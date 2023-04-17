742fe5a88aeb472f6bde7c16bbb3589a7f09d5e3
Edward Tanguay (windows) <edward@tanguay.info>
2023-04-17 10:20:07 +0200

    added employees.json

--- src/server.js
+++ src/server.js
@@ -7,6 +7,10 @@ app.get('/', (req, res) => {
 	res.send('mernbasic API')
 });
 
+app.get('/employees', (req, res) => {
+	res.send('test: employees')
+});
+
 app.listen(port, () => {
 	console.log(`listening at http://localhost:${port}`);
 });
